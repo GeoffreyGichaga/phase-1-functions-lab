f52dfc41bdc5be471e8d2777d2540225ed67e184
Geoffrey Gichaga <geoffrey.githinji@student.moringaschool.com>
2022-04-12 01:36:27 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+// Code your solution in this file! 43 = 1,50=8,34=8,**43=264,50=2112,34=2112
+
+let location = 42
+function distanceFromHqInBlocks(pickup)
+{
+    if(pickup<42)
+    { 
+        distance = 42 - pickup
+        return distance;
+    } else{
+
+        distance = pickup - 42
+    return distance }
+}
+
+function distanceFromHqInFeet(pickup)
+{
+
+    
+
+
+
+
+
+
+}
